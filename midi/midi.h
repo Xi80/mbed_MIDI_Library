e8915353eabbd8bd3361bd12ab83b81523e463c2
Luna Tsukiyono <h31e26@ed.cc.suzuka-ct.ac.jp>
2020-06-17 00:39:19 +0900

    コメント書いた

--- midi/midi.h
+++ midi/midi.h
@@ -4,8 +4,13 @@
 #include "DKS_CircularBuffer.h"
 
 #define USBDEBUG
-// MIDI Library for mbed
-// Made by C0ntra1l
+
+/**
+ * @file midi.h
+ * @brief MIDI library for mbed
+ * @author Luna Tsukiyono
+ * @date 2020/06/16
+ */
 
 
 const static uint8_t gmSystemOn[] = {0x7E,0x7F,0x09,0x01};
@@ -15,19 +20,62 @@ const static uint8_t gsReset[] = {0x41,0x10,0x42,0x12,0x40,0x00,0x7F,0x00,0x41};
 
 class midi{
     public:
+        /** Create a MIDI instance connected to specified UART pins
+        *
+        * @param tx UART TX Pin
+        * @param rx UART RX Pin
+        */
         midi(PinName tx,PinName rx);
 
+    
         void setCallbackNoteOn(void (*func)(int,int,int));
+        /**
+         * @fn
+         * set callback function which is called when the message is note on
+         * @brief set callback function
+         * @param function pointer which arguments is (int channel,int noteNumber,int velocity)
+         * @return void
+         */
         void setCallbackNoteOff(void (*func)(int,int));
+        /**
+         * @fn
+         * set callback function which is called when the message is note off
+         * @brief set callback function
+         * @param function pointer which arguments is (int channel,int noteNumber)
+         * @return void
+         */
         void setCallbackControlChange(void (*func)(int,int,int));
+        /**
+         * @fn
+         * set callback function which is called when the message is controlchange
+         * @brief set callback function
+         * @param function pointer which arguments is (int channel,int controlChangeNumber,int value)
+         * @return void
+         */
         void setCallbackProgramChange(void (*func)(int,int));
+        /**
+         * @fn
+         * set callback function which is called when the message is program change
+         * @brief set callback function
+         * @param function pointer which arguments is (int channel,int programNumber)
+         * @return void
+         */
         void setCallbackReset(void (*func)(void));
-
+        /**
+         * @fn
+         * set callback function which is called when the message is reset
+         * @brief set callback function
+         * @param void
+         * @return void
+         */
         void parse(void);
 
     private:
+
         Serial serial;
+
         DKS::CircularBuffer<uint8_t,512> buffer;
+
         DKS::CircularBuffer<uint8_t,64> sysExBuffer;
 
         void (*noteOnFunc)(int,int,int);
